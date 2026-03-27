03924f72495defb932ecbbafd12b7618181a93d1
wanoh <akosinoah2005@gmail.com>
2026-03-27 11:53:36 +0800

    added a header files for the functions and includes

---
+++ includes.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <thread>
+#include <iostream>
+#include <conio.h>
+#include <random>
+#include <Windows.h>
+#include <string>
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+#define space 32
+#define ESC 27
+bool checkUnder(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
+int getrandom();
+
+void loadEmptyBlock(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
+bool Game_Over(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
+void loadtetris(char(&Design)[22][22], int(&block), short(&pos), short(&pos2),int(&value), char(&key));
+
+void loadBlock(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
+
+void moveBlockright(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
+
+void moveBlockleft(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
+
+void sideways(char(&Design)[22][22], int(&block), short(&pos), short(&pos2), int(&value), char(&key));
+
+void fallBlock(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
+
--- tetris2026-continuation.cpp
+++ tetris2026-continuation.cpp
@@ -1,33 +1,4 @@
-#include <thread>
-#include <iostream>
-#include <conio.h>
-#include <random>
-#include <Windows.h>
-#include <string>
-
-#define KEY_UP 72
-#define KEY_DOWN 80
-#define KEY_LEFT 75
-#define KEY_RIGHT 77
-#define space 32
-#define ESC 27
-bool checkUnder(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
-int getrandom();
-
-
-void loadEmptyBlock(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
-
-void loadtetris(char(&Design)[22][22], int(&block), short(&pos), short(&pos2),int(&value), char(&key));
-
-void loadBlock(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
-
-void moveBlockright(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
-
-void moveBlockleft(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
-
-void sideways(char(&Design)[22][22], int(&block), short(&pos), short(&pos2), int(&value), char(&key));
-
-void fallBlock(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
+#include "includes.h"
 
 int main() {
 
@@ -108,6 +79,7 @@ bool checkUnder(char(&Design)[22][22], int(&block), short(&pos), short(&pos2)) {
     return false;
 }
 
+bool Game_Over(char(&Design)[22][22], int(&block), short(&pos), short(&pos2));
 
 //load empty block or remove block
 void loadEmptyBlock(char(&Design)[22][22], int(&block), short(&pos), short(&pos2)) {
@@ -294,7 +266,7 @@ void sideways(char(&Design)[22][22], int(&block), short(&pos), short(&pos2), int
          key = _getch();
          value = key;
 
-        while (checkUnder(Design, block, pos, pos2)) {
+        while (true) {
 
             switch (key) {
             case KEY_RIGHT:
